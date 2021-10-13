a317fc881736203253014ef0fd3a8cc6cf42bc79
Philip <philipmjobe@gmail.com>
2021-10-13 13:44:35 -0400

    added third product

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 # Product.create(name: "Motorcycle", price: 9799, image_url: "https://cdp.azureedge.net/products/USA/IDN/2021/MC/CRUISER/SCOUT_BOBBER_SIXTY_ABS/49/THUNDER_BLACK/2000000011.jpg", description: "Indian Scout Bobber")
 
-Product.create(name: "Helmet", price: 270, image_url: "https://www.bellhelmets.com/dw/image/v2/BDBJ_PRD/on/demandware.static/-/Sites-bell-master-catalog/default/dw48a91d14/images/large/bell-rogue-cruiser-motorcycle-helmet-honor-matte-titanium-black-right.jpg?sw=800&sh=800&sm=fit)", description: "A helmet for motorcycle riding")
+# Product.create(name: "Helmet", price: 270, image_url: "https://www.bellhelmets.com/dw/image/v2/BDBJ_PRD/on/demandware.static/-/Sites-bell-master-catalog/default/dw48a91d14/images/large/bell-rogue-cruiser-motorcycle-helmet-honor-matte-titanium-black-right.jpg?sw=800&sh=800&sm=fit)", description: "A helmet for motorcycle riding")
+
+Product.create(name: "Motorcycle Jacket", price: 1009, image_url: "https://www.schottnyc.com/images/source/618_BRN_FRT_NEW2.jpg)", description: "a jacket to wear while riding motorcycles")
