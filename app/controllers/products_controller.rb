a3b9773677422f43cb8f8822019e8c480c738056
Philip <philipmjobe@gmail.com>
2021-10-13 14:08:00 -0400

    Added routes and controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   def all_product 
-    all_product = Product.first
-    render json: all_product
+    all_product = Product.all
+    render json:  all_product.as_json
   end 
 end
