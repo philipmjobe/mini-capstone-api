6fe8ffadfc60edc81ea23b0a5558577160536810
Philip <philipmjobe@gmail.com>
2021-10-15 14:04:56 -0400

    added post route and create controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,10 +5,20 @@ class ProductsController < ApplicationController
     render json: product
   end 
 
-
   def show 
     product_id = params["id"].to_i
     product = Product.find_by id: product_id
+    render json: product.as_json
+  end 
+
+  def create 
+    product = Product.new(
+      name: params["name"],
+      price: params["price"],
+      image_url: params["image_url"],
+      description: params["description"]
+    )
+    product.save
     render json: product.as_json
   end 
 end
