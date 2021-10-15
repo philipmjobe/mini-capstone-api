2ceb54b0edcfe760450108845390123e3990cd83
Philip <philipmjobe@gmail.com>
2021-10-15 14:54:50 -0400

    fixed indentation in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-get "/products" => "products#index"
-get "/products/:id" => "products#show"
-post "/products" => "products#create"
-patch "/products/:id" => "products#update"
-delete "/products/:id" => "products#destroy"
+  get "/products" => "products#index"
+  get "/products/:id" => "products#show"
+  post "/products" => "products#create"
+  patch "/products/:id" => "products#update"
+  delete "/products/:id" => "products#destroy"
 end
