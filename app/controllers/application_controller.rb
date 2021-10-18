41076a7510b76a0c9cb34dead25733ab99e33732
Philip <philipmjobe@gmail.com>
2021-10-18 15:08:41 -0400

    added serializeer gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,14 @@
 class ApplicationController < ActionController::API
 
   def is_discounted
-    if price < 10
-      return true
-    else
-      return false
-    end
+    price <= 10
   end 
 
   def tax
-    tax = price *0.09
-    return tax
+    tax = price * 0.09
   end 
 
   def total
     total = tax + price
-    return total
   end 
 end
