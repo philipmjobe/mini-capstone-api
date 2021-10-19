ce78c2acaccdbe92a2ddfc5216e9b18118b2b603
Philip <philipmjobe@gmail.com>
2021-10-19 15:12:13 -0400

    changed integer to decimal in the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_19_190103) do
+ActiveRecord::Schema.define(version: 2021_10_19_190913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.integer "price"
+    t.decimal "price", precision: 9, scale: 2
     t.string "image_url"
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
