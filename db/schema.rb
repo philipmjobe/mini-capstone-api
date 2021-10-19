4078ee5d9d3f34659984a678a07c390fe701f330
Philip <philipmjobe@gmail.com>
2021-10-19 15:15:47 -0400

    changed description from string to text in the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_19_190913) do
+ActiveRecord::Schema.define(version: 2021_10_19_191334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2021_10_19_190913) do
     t.string "name"
     t.decimal "price", precision: 9, scale: 2
     t.string "image_url"
-    t.string "description"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "inventory", default: 0
