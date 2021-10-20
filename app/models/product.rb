20896e5750081d3756297c8f127e26fb90615186
Philip <philipmjobe@gmail.com>
2021-10-20 13:12:28 -0400

    added validations to product.rb model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ApplicationRecord
+  validates :name, length: {minimum: 2}
+  validates :price, presence: true, numericality: {only_integer: true}
+  validates :image_url, presence: true 
+  validates :description, presence: true, length: {maximum 500}
+
+
   def is_discounted
     price <= 10 
   end 
