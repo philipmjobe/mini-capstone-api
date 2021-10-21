0a1974911908db88d7dc3a36d1628f1cbbca341a
Philip <philipmjobe@gmail.com>
2021-10-21 13:53:10 -0400

    dumped the database into seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-# Product.create(name: "Motorcycle", price: 9799, image_url: "https://cdp.azureedge.net/products/USA/IDN/2021/MC/CRUISER/SCOUT_BOBBER_SIXTY_ABS/49/THUNDER_BLACK/2000000011.jpg", description: "Indian Scout Bobber")
-
-# Product.create(name: "Helmet", price: 270, image_url: "https://www.bellhelmets.com/dw/image/v2/BDBJ_PRD/on/demandware.static/-/Sites-bell-master-catalog/default/dw48a91d14/images/large/bell-rogue-cruiser-motorcycle-helmet-honor-matte-titanium-black-right.jpg?sw=800&sh=800&sm=fit)", description: "A helmet for motorcycle riding")
-
-#Product.create(name: "Motorcycle Jacket", price: 1009, image_url: "https://www.schottnyc.com/images/source/618_BRN_FRT_NEW2.jpg)", description: "a jacket to wear while riding motorcycles")
+# Supplier.create!([
+#   {name: "Indian Motorcycles", email: "i@gmail.com", phone_number: "8675309"},
+#   {name: "Bell Helmets", email: "b@gmail.com", phone_number: "2223333"},
+#   {name: "Schott NYC", email: "s@gmail.com", phone_number: "4453434"}
+# ])
+# Product.create!([
+#   {name: "Motorcycle", price: "9799.0", image_url: "https://cdp.azureedge.net/products/USA/IDN/2021/MC/CRUISER/SCOUT_BOBBER_SIXTY_ABS/49/THUNDER_BLACK/2000000011.jpg", description: "Indian Scout Bobber", inventory: 0},
+#   {name: "Helmet", price: "270.0", image_url: "https://www.bellhelmets.com/dw/image/v2/BDBJ_PRD/on/demandware.static/-/Sites-bell-master-catalog/default/dw48a91d14/images/large/bell-rogue-cruiser-motorcycle-helmet-honor-matte-titanium-black-right.jpg?sw=800&sh=800&sm=fit)", description: "A helmet for motorcycle riding", inventory: 0},
+#   {name: "Motorcycle Jacket", price: "1009.0", image_url: "https://www.schottnyc.com/images/source/618_BRN_FRT_NEW2.jpg)", description: "a jacket to wear while riding motorcycles", inventory: 0},
+#   {name: "Motorcycle boots", price: "985.0", image_url: "https://www.revzilla.com/product_images/0125/8230/forma_adventure_boots_300x300.jpg", description: "These keep your feet safe while riding a mototrcycle", inventory: 0},
+#   {name: "Dainese Blackjack Gloves", price: "125.0", image_url: "https://www.revzilla.com/product_images/0217/2147/dainese_blackjack_gloves_300x300.jpg", description: "Timeless Italian taste, quality leather construction and nothing more. The Dainese Blackjack Gloves give off good vibes for the stylish summer rider.", inventory: 0}
+])
