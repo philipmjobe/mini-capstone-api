722a8004d8f2e01b17c775914493ca28ad396981
Philip <philipmjobe@gmail.com>
2021-10-21 17:29:28 -0400

    added to suppliers_controller.rb

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -17,5 +17,33 @@ class SuppliersController < ApplicationController
       email: params["email"]
       phone_number: params["phone_number"]
     )
+    if supplier.save
+      render json: supplier 
+    else 
+      render json: {errors: supplier.errors.full_messages}, status: 406
+    end 
+  end 
+
+  def update
+    supplier_id = params["id"].to_i
+    supplier = Supplier.find_by id: supplier_id
+
+    supplier.name = params["name"] || supplier.name
+    supplier.email = params["email"] || supplier.email
+    supplier.phone_number = params["phone_number"] || supplier.phone_number
+
+    if supplier.save
+      render json: supplier 
+    else 
+      render json: {errors: supplier.errors.full_messages}, status: 406
+    end 
+  end 
+
+  def destroy
+    supplier_id = params["id"].to_i
+    supplier = Supplier.find_by id: supplier_id
+
+    supplier.destroy
+    render json: {message: "Sucessfully Destroyed"}
   end 
 end
