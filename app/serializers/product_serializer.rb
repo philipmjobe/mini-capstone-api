d320d2409e15496b129b7a86b4ebb0e9098a64fe
Philip <philipmjobe@gmail.com>
2021-10-21 18:07:19 -0400

    added belongs_to and has_many to product_serializer.rb

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,3 +1,6 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :name, :price, :images, :description,:is_discounted, :tax, :total, :supplier
+  attributes :id, :name, :price, :images, :description, :is_discounted, :tax, :total
+
+  belongs_to :supplier
+  has_many :images 
 end
