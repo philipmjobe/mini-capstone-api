380b177fa1e5abadc568e05e9c982e6c57a51404
Philip <philipmjobe@gmail.com>
2021-10-22 09:52:47 -0400

    added images to products and updated serializer.rb

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,5 +1,5 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :name, :price, :images, :description, :is_discounted, :tax, :total
+  attributes :id, :name, :price, :description, :is_discounted, :tax, :total
 
   belongs_to :supplier
   has_many :images 
