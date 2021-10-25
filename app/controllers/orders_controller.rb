b8e0eb55fda1da79bd22e29a01cf81b1b6db4857
Philip <philipmjobe@gmail.com>
2021-10-25 15:05:09 -0400

    added if current user to create method in orders_controller.rb

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -18,19 +18,23 @@ class OrdersController < ApplicationController
   
   
   def create 
-    order = Order.new(
-      user_id: current_user.id,
-      product_id: params["product_id"],
-      quantity: params["quanttity"],
-      subtotal: params["subtotal"],
-      tax: params["tax"],
-      total: params["total"]
-    )
+    if current_user
+      order = Order.new(
+        user_id: current_user.id,
+        product_id: params["product_id"],
+        quantity: params["quanttity"],
+        subtotal: params["subtotal"],
+        tax: params["tax"],
+        total: params["total"]
+      )
 
-    if order.save 
-      render json: order 
-    else  
-      render json: {errors: order.errors.full_messages}, status: 406
+      if order.save 
+        render json: order 
+      else  
+        render json: {errors: order.errors.full_messages}, status: 406
+      end 
+    else 
+      render json: [], status: 406 
     end 
   end 
 end
