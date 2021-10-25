9d74b4f31f85a5cac1dc00bb180865c95df42dd5
Philip <philipmjobe@gmail.com>
2021-10-25 14:48:18 -0400

    added to order_serializer.rb

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,4 +1,5 @@
 class OrderSerializer < ActiveModel::Serializer
-  attributes :id, :product_id, :user_id, :quantity,
-  :subtotal, :tax, :total 
+  attributes :id, :quantity, :subtotal, :tax, :total 
+
+  belongs_to :product 
 end
