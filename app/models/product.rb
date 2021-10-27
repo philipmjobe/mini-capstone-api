d77c58cc1ada22b9df982f094f6fad4c4a3bacba
Philip <philipmjobe@gmail.com>
2021-10-27 17:26:42 -0400

    updated serializers and models

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,16 +3,13 @@ class Product < ApplicationRecord
   has_many :categories, through: :category_products
   belongs_to :supplier
   has_many :orders
+  has_many :images 
   
   validates :name, presence: true, length: {minimum: 2}
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :image_url, presence: true 
   validates :description, presence: true, length: { in: 10..500 }
   
-  def images
-    Image.where(product_id: id)
-  end 
-
   def is_discounted
     price <= 10 
   end 
--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,7 +1,8 @@
 class ProductSerializer < ActiveModel::Serializer
   
-  attributes :id, :name, :price, :description, :is_discounted, :tax, :total
+  attributes :id, :name, :price, :description, :is_discounted, :tax, :total, :inventory
 
   belongs_to :supplier
   has_many :images 
+  has_many :categories
 end
