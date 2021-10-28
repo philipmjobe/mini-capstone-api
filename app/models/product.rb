a4773891f81dd92b8e41f2f75ddb9054d7bd9a01
Philip <philipmjobe@gmail.com>
2021-10-28 13:16:39 -0400

    added routes and controller for carted_products

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,8 @@ class Product < ApplicationRecord
   has_many :category_products
   has_many :categories, through: :category_products
   belongs_to :supplier
-  has_many :orders
+  has_many :carted_products
+  has_many :orders, through: :carted_products
   has_many :images 
   
   validates :name, presence: true, length: {minimum: 2}
--- config/routes.rb
+++ config/routes.rb
@@ -31,4 +31,8 @@ Rails.application.routes.draw do
   get "/orders" => "orders#index"
   get "/orders/:id" => "orders#show"
   post "/orders" => "orders#create"
+
+
+  ### CARTED PRODUCTS
+  post "/carted_products" => "carted_products#create"
 end
