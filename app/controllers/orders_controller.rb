fe30b014b7e2f297cf363bd0e934e06f00593124
Philip <philipmjobe@gmail.com>
2021-10-28 17:40:38 -0400

    fixed index show and create methods in orders_controller.rb to match the demo_mini_capstone

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,46 +2,30 @@ class OrdersController < ApplicationController
   before_action :authenticate_user
 
   def index
-    if current_user
-      orders = current_user.orders 
-      render json: orders 
-    else  
-      render json: [], status: 406
-    end   
+    orders = current_user.orders
+    render json: orders, include: "carted_products.product"  
   end 
   
   
   def show 
     order = current_user.orders.find_by(id: params[:id])
-    render json: order 
+    render json: orders, include: "carted_products.product" 
   end 
   
   
   
-  def create 
-    product = Product.find_by(id: params[:product_id])
+  def create
+    order = Order.new(
+      user_id: current_user.id
+    ) 
 
-    calculated_subtotal = product.price * params[:quantity]
-    calculated_tax = calculated_subtotal * 0.09
-    calculated_total = calculated_subtotal * calculated_tax
-    
-    if current_user
-      order = Order.new(
-        user_id: current_user.id,
-        product_id: params["product_id"],
-        quantity: params["quanttity"],
-        subtotal: calculated_subtotal,
-        tax: calculated_tax,
-        total: calculated_total
-      )
-
-      if order.save 
-        render json: order 
-      else  
-        render json: {errors: order.errors.full_messages}, status: 406
-      end 
-    else 
-      render json: [], status: 406 
+    if order.save
+      carted_products = current_user.carted_products.where(status: "carted")
+      carted_products.update_all(status: "purchased", order_id: order.id)
+      order.update_totals
+      render json: order, include: "carted_products.product"
+    else  
+      render json: {errors: order.errors.full_messages}, status: 406
     end 
-  end 
+  end    
 end
