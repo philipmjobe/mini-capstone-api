aba1b5a113560818d300b537a74bcbef989ce920
Philip <philipmjobe@gmail.com>
2021-10-28 17:59:46 -0400

    fixed user.rb to match demo_mini_capstone

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
   has_many :orders 
+  has_many :carted_products
   has_secure_password
   validates :email, presence: true, uniqueness: true
-  
-  has_many :carted_products
-  has_many :orders, through: :carted_products
 end
