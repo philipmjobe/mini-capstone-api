b71c1ec0c776fc9d6a14906ee354c139f4717e9a
Philip <philipmjobe@gmail.com>
2021-10-28 13:35:05 -0400

    added a carted products create to request.http and created the first product

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,2 +1,17 @@
 class CartedProductsController < ApplicationController
+  def create
+    carted_products = CartedProduct.new(
+      user_id: params[:user_id],
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      status: params[:status],
+      order_id: params[:order_id]
+    )
+    if carted_products.save
+      render json: {message: "Suceccfully Created"},
+      status: :created
+    else
+      render json: {errors: carted_products.errors.full_messages}, status: 406
+    end
+  end 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   has_many :orders 
   has_secure_password
   validates :email, presence: true, uniqueness: true
-
+  
+  has_many :carted_products
+  has_many :orders, through: :carted_products
 end
